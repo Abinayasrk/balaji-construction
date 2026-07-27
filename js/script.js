19da8559581ea11c42010ce44fc71640f1851775
Abinayasrk <abi96.ece@gmail.com>
2026-07-27 17:48:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+const fadeElements = document.querySelectorAll('.fade-section');
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show'); // வெளியே போகும்போது remove
+        }
+    });
+}, {
+    threshold: 0.3
+});
+
+fadeElements.forEach(el => observer.observe(el));
