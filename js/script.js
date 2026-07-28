f561e31aa201d2f164e837f09db8b6b414ed9a1b
Abinayasrk <abi96.ece@gmail.com>
2026-07-28 18:56:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,6 +35,8 @@ reveals.forEach((el)=>{
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 
+if(hamburger){
+
 hamburger.addEventListener("click",()=>{
 
     navLinks.classList.toggle("active");
@@ -47,3 +49,5 @@ hamburger.addEventListener("click",()=>{
     }
 
 });
+
+}
