ca610b05bac336c66fed298e40c4ff0448dcff38
Abinayasrk <abi96.ece@gmail.com>
2026-07-28 18:36:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,19 @@ function toggleMenu() {
 reveals.forEach((el)=>{
     observer.observe(el);
 });
+
+//navbar mobile script
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+const navItems = document.querySelectorAll("#nav-links a");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Menu item click ஆனவுடன் menu close ஆகும்
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
